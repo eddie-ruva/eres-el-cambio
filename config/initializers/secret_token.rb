5b83556ceefa5d08443ceda496cd6261f22dc7f2
Edilberto Ruvalcaba <eddie.ruva@gmail.com>
2013-02-10 17:29:36 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EresElCambio::Application.config.secret_token = 'a50c5cd788ad884b6a62d34fb8929d958e0cc332032980d87e96112ea7ad25074db05c975aa3cce23d8e89a242c673f9b2dd06db4e0053277005b4f2fbd697c9'
