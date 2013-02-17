522211513b65b43d07bbf7a4a93593804f775136
Edilberto Ruvalcaba <eddie.ruva@gmail.com>
2013-02-16 20:10:43 -0600

    Added classifier by county to requester and changed test file name

--- lib/cic_requester.rb
+++ lib/cic_requester.rb
@@ -10,9 +10,32 @@ class CicRequester
     if group_name
       group_id = GROUPS[group_name.to_sym]
       options.merge!({:for_group => group_id})
-      get('/0/nl/reports.json', :query => options)
+      classify_per_county get('/0/nl/reports.json', :query => options)
     else
       {}
     end
   end
+
+
+  private
+
+  def self.classify_per_county(reports)
+    reports_by_county = {}
+    reports['reports'].each do |report|
+      county = report['address_detail']['county']['long_name']
+      if county
+        county.downcase!
+        mini_report = { :ticket => report['ticket'],
+                        :state => report['state'],
+                        :group => report['group'].downcase}
+        if reports_by_county[county.to_sym]
+          reports_by_county[county.to_sym] << mini_report
+        else
+          reports_by_county[county.to_sym] = [mini_report]
+        end
+      end
+    end
+    reports_by_county
+  end
+
 end
